c121f2c5e54e0ddb931c444b12eee9b9f2e212e9
Rodrigo <rodrigoevaldes@hotmail.com>
2016-01-17 14:20:03 -0300

    Se agrega AngularJs.

---
+++ Angularoliticos/Models/Infraestructura/CloudProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angularoliticos.Models.Infraestructura
+{
+    class CloudProvider
+    {
+        public int CloudProviderID { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String WebsiteUrl { get; set; }
+
+
+
+
+
+    }
+}
